e9ec2948f5e6a406328fbedc070aa216a2cf23f1
vadimkaKharitonenko <vadimka.kharitonenko@gmail.com>
2020-04-17 21:24:29 +0700

    try plugins for scroll

--- src/components/main-indicator/index.ts
+++
@@ -1,9 +0,0 @@
-import './index.css';
-import $ from 'jquery';
-
-$(function() {
-    $(document).delegate('.js-main-indicator .main-indicator__dot', 'click', function() {
-        $('.js-main-indicator .main-indicator__dot').removeClass('active');
-        $(this).addClass('active');
-    })
-});
