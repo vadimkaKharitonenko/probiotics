f14d209f66b1e7ae119f8acfb3a06167bdb68ff9
vadimkaKharitonenko <vadimka.kharitonenko@gmail.com>
2020-04-17 18:03:59 +0700

    add main contacts

---
+++ src/components/main-contacts/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import './index.css';
+
+$(function() {
+    ymaps.ready(init);
+    function init() {
+        const contactsMap = new ymaps.Map("contactsMap", {
+            center: [55.76, 37.64],
+            zoom: 10
+        });
+
+        const placemark = new ymaps.Placemark([55.76, 37.56], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: require('../../assets/img/placemark.png'),
+            iconImageSize: [35, 44],
+            iconImageOffset: [-3, -42]
+        });
+
+        contactsMap.geoObjects.add(placemark);
+    }
+})
