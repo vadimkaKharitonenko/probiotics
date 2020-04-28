b58c74f6ea7b713f161b4cfdb90223fd2cd265e5
vadimkaKharitonenko <vadimka.kharitonenko@gmail.com>
2020-04-28 14:32:32 +0700

    add adaptive for partners page

--- src/pages/partners/index.ts
+++ src/pages/partners/index.ts
@@ -1,1 +1,21 @@
 import './index.css';
+import $ from 'jquery';
+
+$(function() {
+  const isMobile = document.documentElement.clientWidth <= 768;
+
+  if (isMobile) {
+    $('.js-partners-items').slick({arrows: false});
+  }
+
+  $('.js-partners-prev').on('click', function() {
+    const slider = $(this).closest('.partners__controls').prev();
+    $(slider).slick('slickPrev');
+  })
+
+  $('.js-partners-next').on('click', function() {
+    const slider = $(this).closest('.partners__controls').prev();
+    $(slider).slick('slickNext');
+  })
+
+});
