2ea6d8072744aa3b63b12a8b91d8c6aa4a6724da
vadimkaKharitonenko <vadimka.kharitonenko@gmail.com>
2020-04-28 13:15:30 +0700

    add top banner scrolling

--- src/components/top-banner/index.ts
+++ src/components/top-banner/index.ts
@@ -5,4 +5,18 @@ $(function() {
   if (document.documentElement.clientWidth <= 768) {
     document.querySelector('.top-banner__menu a.active')?.scrollIntoView();
   }
+
+  function scrollHorizontally(e : any) {
+    e = window.event || e;
+    const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    const element = document.querySelector('.top-banner__menu');
+    if (!element) return;
+    element.scrollLeft -= (delta*40); 
+    e.preventDefault();
+  }
+
+  const topBannerMenu = document.querySelector('.top-banner__menu');
+
+  topBannerMenu?.addEventListener("mousewheel", scrollHorizontally, false);
+  topBannerMenu?.addEventListener("DOMMouseScroll", scrollHorizontally, false);
 })
