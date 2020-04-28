8b947c1a7cf416a74ae3d8e8593254f0536bace8
vadimkaKharitonenko <vadimka.kharitonenko@gmail.com>
2020-04-28 17:20:24 +0700

    add adaptive for history page

--- src/components/history-slider/index.ts
+++ src/components/history-slider/index.ts
@@ -16,16 +16,23 @@ $(function () {
 
   function initHistorySlider() {
     const width: number = document.documentElement.clientWidth;
+    console.log(width);
 
     if (width >= 1900) {
       return $('.js-history-slider-years').scrollLeft(155);
     }
 
-    if (width <= 1600) {
+    if (width >= 1600) {
       return $('.js-history-slider-years').scrollLeft(155 * 2 - 2);
     }
 
+    if (width >= 1425) {
+      return $('.js-history-slider-years').scrollLeft(155 * 3 - 70);
+    }
 
+    if (width >= 768) {
+      return $('.js-history-slider-years').scrollLeft(0);
+    }
   }
 
   $(document).delegate('.js-history-year', 'click', function () {
@@ -41,6 +48,16 @@ $(function () {
     if (width <= 1600) {
       addiction = 1;
       offset = 2;
+    }
+
+    if (width <= 1425) {
+      addiction = 1;
+      offset = -85;
+    }
+
+    if (width <= 768) {
+      addiction = 0;
+      offset = 75 * index;
     }
 
     $('.js-history-slider-years').scrollLeft(155 * (index + addiction) - offset);
